26be6ff5bd978f51758637fed9e18ef345c6d82a
abelfy <75290328+abelfynine@users.noreply.github.com>
2024-06-21 01:44:30 -0600

    Update main.jsx

--- ReactJs/weather-app/src/main.jsx
+++ ReactJs/weather-app/src/main.jsx
@@ -1,8 +1,11 @@
+// Se importan las bibliotecas necesarias de React para la renderización del componente principal de la aplicación.
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// Se utiliza el método createRoot de ReactDOM para renderizar el componente principal de la aplicación en el elemento con el id 'root' del DOM.
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // Se envuelve la aplicación en <React.StrictMode> para activar advertencias adicionales y verificar problemas de rendimiento en el código.
   <React.StrictMode>
     <App />
   </React.StrictMode>,
